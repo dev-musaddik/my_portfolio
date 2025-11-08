ba6b5f578df4513f4b7642a88c68324d5e74acb9
Musaddik Hossain <musaddikh13email.com>
2025-11-08 15:49:27 +0600

    upload code link

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create an axios instance
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // Set the base URL for all requests
+  // baseURL: 'http://localhost:5001/api', // Set the base URL for all requests
+  baseURL: 'https://my-portfolio-backend-three-sand.vercel.app/api', // Set the base URL for all requests
   timeout: 10000,  // Optional: Set a timeout for requests (in ms)
   headers: {
     'Content-Type': 'application/json',  // Default header
